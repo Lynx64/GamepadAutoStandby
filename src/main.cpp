e66ee4c38e0924f85c12ff799bdc308a0e704f73
Andrew Hawes <117602754+Andrew1Hawes@users.noreply.github.com>
2024-08-11 04:05:44 +0100

    Lower power off on boot uptime check to 1 min

--- src/main.cpp
+++ src/main.cpp
@@ -17,28 +17,37 @@ WUPS_USE_WUT_DEVOPTAB();
 
 extern "C" uint32_t OSGetBootPMFlags(void);
 
+static void shutdownGamePadOnBoot()
+{
+    uint32_t bootFlags = OSGetBootPMFlags();
+    /*  Check that the GamePad wasn't used to turn on the console and
+        this isn't an OS relaunch (OSForceFullRelaunch()).
+        (Starting with 0) Bit 1 or 27 - GamePad used to turn on the console.
+        Bit 13 - OS relaunch (OSForceFullRelaunch()).
+        See more https://wiiubrew.org/wiki/Boot1#PowerFlags */
+    if ((bootFlags & 0x8002002) > 0) {
+        return;
+    }
+
+    // Determine whether there has been a plugin reload by checking OS uptime
+    // Get uptime in ticks and convert to seconds
+    OSTime uptime = OSTicksToSeconds(OSGetSystemTime());
+    if (uptime > 60) {
+        return;
+    }
+    
+    //then shutdown GamePad
+    DEBUG_FUNCTION_LINE_INFO("Shutting down GamePad on boot (BootPMFlags: 0x%08X , Uptime: %lld secs)", bootFlags, uptime);
+    CCRCDCSysConsoleShutdownInd(CCR_CDC_DESTINATION_DRC0);
+    gInShutdown = true;
+}
+
 // Gets called ONCE when the plugin was loaded
 INITIALIZE_PLUGIN()
 {
     initConfig();
     
     if (gShutdownOnBoot) {
-        uint32_t bootFlags = OSGetBootPMFlags();
-        /*  Check that the GamePad wasn't used to turn on the console and
-            this isn't an OS relaunch (OSForceFullRelaunch()).
-            (Starting at 0) Bit 1 or 27 - GamePad used to turn on the console.
-            Bit 13 - OS relaunch (OSForceFullRelaunch()).
-            See more https://wiiubrew.org/wiki/Boot1#PowerFlags */
-        if ((bootFlags & 0x8002002) == 0) {
-            // Determine whether there has been a plugin reload by checking OS uptime
-            // Get uptime in ticks and convert to seconds
-            OSTime uptime = OSTicksToSeconds(OSGetSystemTime());
-            if (uptime < 300) { //super lenient
-                //then shutdown GamePad
-                DEBUG_FUNCTION_LINE_INFO("Shutting down GamePad on boot (BootPMFlags: 0x%08X , Uptime: %lld secs)", bootFlags, uptime);
-                CCRCDCSysConsoleShutdownInd(CCR_CDC_DESTINATION_DRC0);
-                gInShutdown = true;
-            }
-        }
+        shutdownGamePadOnBoot();
     }
 }
